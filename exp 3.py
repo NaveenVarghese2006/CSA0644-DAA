b5fcd41b55ed5a28b1e577264445fe1a87b2347a
Naveen Varghese <naveenvar2006@gmail.com>
2026-04-10 09:10:19 +0530

    Create exp 3.py

---
+++ exp 3.py
@@ -0,0 +1,16 @@
+def sum_of_squares(nums):
+    total = 0
+    n = len(nums)
+    
+    for i in range(n):
+        unique_seen = set()
+        for j in range(i, n):
+            unique_seen.add(nums[j])
+            
+            count = len(unique_seen)
+            
+            total += (count * count)
+            
+    return total
+
+print(sum_of_squares([1, 2, 1]))
